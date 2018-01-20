35b96cd48fcd865be131d245f292e82c48b6944e
cristianacmc <cristianacmc@gmail.com>
2018-01-20 21:19:20 +0000

    Add serialized data in the database

--- database.py
+++ database.py
@@ -7,12 +7,20 @@ from sqlalchemy import create_engine
  
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
 class Category(Base):
     __tablename__ = 'category'
    
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
+
     
 
 class CategoryItem(Base):
@@ -24,6 +32,20 @@ class CategoryItem(Base):
     description = Column(String(250))
     category_id = Column(Integer,ForeignKey('category.id'))
     category = relationship(Category)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        #Return Data Serialized#
+        return {
+            'id' : self.id,
+            'name': self.name, 
+            'description': self.description,
+            'picture': self.picture,
+            'category': self.category.name            
+        }
+
     
 
 
