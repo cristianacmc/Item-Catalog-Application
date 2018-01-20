f1035e55db81f06a8439123a84e55baf40eb2420
cristianacmc <cristianacmc@gmail.com>
2018-01-20 21:56:14 +0000

    Fix gconnect function

--- database.py
+++ database.py
@@ -3,16 +3,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
-
  
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+    email = Column(String(250), nullable = False)
     picture = Column(String(250))
 
 class Category(Base):
